d7a22c1c3cb9b1baadfe67ae26507bd2e5eee80e
Evan Thomas <evan.thomas@epfl.ch>
2026-05-08 15:31:43 +0200

    Delete old sync workspace members, now in their own repos

--- river-data-sync-common/src/error.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, thiserror::Error)]
-pub enum RiverDataClientError {
-    #[error("HTTP error: {0}")]
-    Http(#[from] reqwest::Error),
-    #[error("API error: {0}")]
-    Api(String),
-}
--- river-data-sync-common/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod client;
-pub mod commands;
-pub mod error;
-pub mod models;
-pub mod river_data_client;
-pub mod runner;
